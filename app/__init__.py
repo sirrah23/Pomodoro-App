47e7cdd1f8364c78ba5ee11e8e2779634940c786
Harris <hseth93@gmail.com>
2017-09-25 23:13:32 -0400

    Added a temporary login-screen. Still a W.I.P

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+from flask_login import LoginManager, login_required
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "mysecretkey"
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.init_app(app)
+
+from . import views
---
+++ app/views.py
@@ -0,0 +1,33 @@
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_required, login_user, UserMixin
+from app import app, login_manager
+
+from .forms import LoginForm
+
+@login_manager.user_loader
+def load_user(id):
+    return TempUser()
+
+class TempUser(UserMixin):
+    
+    id = 1
+
+    def get_id(self):
+        return self.id
+
+@app.route('/')
+@login_required
+def index():
+    return render_template('index.html')
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        if username:
+            # TODO: Add username and password checks
+            login_user(TempUser(), False)
+            return redirect(url_for('index'))
+    flash("Try again")
+    return render_template("login.html", form=form)
