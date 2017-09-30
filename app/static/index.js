0452fb27133033e3e4727b8ee50d0bbe4a8ab1aa
Harris <hseth93@gmail.com>
2017-09-30 19:21:37 -0400

    Added a toggle button + indicator

--- app/static/index.js
+++ app/static/index.js
@@ -1,6 +1,15 @@
 const clock = cf($('.your-clock'));
 clock.generate_clock();
 
+function toggle_mode_indicator(){
+  const indicator = $('#timermode')[0];
+  if (indicator.innerHTML == "Pomodoro"){
+    indicator.innerHTML = "Break";
+  } else {
+    indicator.innerHTML = "Pomodoro";
+  }
+}
+
 $('.start-button').click(function(){
   clock.clock.start();
   $('.context').prop('readonly', true);
@@ -12,8 +21,15 @@ $('.stop-button').click(function(){
 
 $('.reset-button').click(function(){
   clock.clock.stop(function(){
-    clock.toggle_mode();
     clock.reset();
     $('.context').prop('readonly', false);
+  });
+});
+
+$('.toggle-button').click(function(){
+  clock.clock.stop(function(){
+    clock.toggle_mode();
+    clock.reset();
+    toggle_mode_indicator();
   });
 });
