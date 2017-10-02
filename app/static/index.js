9b939613009194b70f0bbe94eec56a459c15023b
Harris <hseth93@gmail.com>
2017-10-02 00:42:59 -0400

    Made a bunch of UI changes
    There is now an interruption counter and
    the code was refactored so that some HTML
    classes became ids.

--- app/static/index.js
+++ app/static/index.js
@@ -1,4 +1,4 @@
-const clock = cf($('.your-clock'));
+const clock = cf($('#clock'));
 clock.generate_clock();
 
 function toggle_mode_indicator(){
@@ -10,32 +10,48 @@ function toggle_mode_indicator(){
   }
 }
 
-clock.add_end_listener(() => {
+function clock_hit_zero_update_ui(){
   setTimeout(() => {
     toggle_mode_indicator();
     clock.toggle_mode();
     clock.reset();
     clock.clock.start();
   }, 2000);
-});
+}
 
-$('.start-button').click(function(){
+function gather_and_send_pomdoro(){
+  const context = $('#context').val();
+  const interruptions = parseInt($('#interruptions').val(), 10);
+  const payload = {context, interruptions};
+  $.post('/pomodoro', payload);
+}
+
+function reset_interruption_counter(){
+  $('#interruptions').val("0");
+}
+
+//Actions to perform once the clock hits zero
+clock.add_end_listener(clock_hit_zero_update_ui);
+clock.add_end_listener(gather_and_send_pomdoro);
+clock.add_end_listener(reset_interruption_counter);
+
+$('#start-button').click(function(){
   clock.clock.start();
   $('.context').prop('readonly', true);
 });
 
-$('.stop-button').click(function(){
+$('#stop-button').click(function(){
   clock.clock.stop();
 });
 
-$('.reset-button').click(function(){
+$('#reset-button').click(function(){
   clock.clock.stop(function(){
     clock.reset();
     $('.context').prop('readonly', false);
   });
 });
 
-$('.toggle-button').click(function(){
+$('#toggle-button').click(function(){
   clock.clock.stop(function(){
     clock.toggle_mode();
     clock.reset();
