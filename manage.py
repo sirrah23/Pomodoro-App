4d7d184277dc7ed5f73966eabc4e6426b4a6a1cc
Harris <hseth93@gmail.com>
2017-10-24 23:48:54 -0400

    Created an adduser command

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from flask_script import Manager, Server, prompt_bool
+from flask_script import Manager, Server, prompt_bool, prompt, prompt_pass
 from project.app import app, db
 from project.app.models import User
 
@@ -16,6 +16,13 @@ def dropdb():
     if prompt_bool("Are you sure you want to delete the database?"):
         db.drop_all()
         print("Dropped the database")
+
+@manager.command
+def adduser():
+    username = prompt("Username?")
+    password = prompt_pass("Password?")
+    db.session.add(User(username=username, password=password))
+    print("Created the new user: " + username)
 
 if __name__ == "__main__":
     manager.run()
